4691f90d680326783d7d8f2e05755b74fd1c3ffa
vm-laptop <wolfmariev@gmail.com>
2020-03-08 14:15:53 +0500

    Поменял дериктории общедоступных файлов. Легкий марафет server

--- webpack.config.client.js
+++ webpack.config.client.js
@@ -1,49 +1,49 @@
 const path = require('path');
 
 const webpack = require('webpack');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const { StatsWriterPlugin } = require('webpack-stats-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const {StatsWriterPlugin} = require('webpack-stats-plugin');
 const CompressionPlugin = require('compression-webpack-plugin');
 
 module.exports = {
   mode: 'production',
   entry: './src/client.js',
   output: {
-      path: path.resolve(__dirname, 'dist', 'public'),
-      filename: 'client.[chunkhash].js'
+    path: path.resolve(__dirname, 'public'),
+    filename: 'client.[chunkhash].js',
   },
   module: {
-      rules: [
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+
+      },
+      {
+        test: /\.css$/,
+        use: [
           {
-              test: /\.(js|jsx)$/,
-              exclude: /node_modules/,
-              use: 'babel-loader',
-              
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: '/',
+            },
           },
-          {
-            test: /\.css$/,
-            use:[
-                {
-                    loader:MiniCssExtractPlugin.loader,
-                    options:{
-                        publicPath:'/'
-                    },
-                },                
-                'css-loader']
-          },
-          {
-            test: /\.(png|svg|jpg|gif)$/,
-            use: [{loader:'file-loader'}]
-            
-            // options:{
-            //     name:'media/favicon.png',
-            //     outputPath:'media'
-            //   }
-            
-          },
-          
-      ]
+          'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [{loader: 'file-loader'}],
+
+        // options:{
+        //     name:'media/favicon.png',
+        //     outputPath:'media'
+        //   }
+
+      },
+
+    ],
   },
   plugins: [
 
@@ -53,22 +53,22 @@ module.exports = {
 
     new webpack.DefinePlugin({
       'process.env': {
-        NODE_ENV: JSON.stringify('production')
-      }
+        NODE_ENV: JSON.stringify('production'),
+      },
     }),
     new StatsWriterPlugin({
-        filename: '../stats.json',
-        stats: {
-          all: false,
-          assets: true,
-          
-        }
-      }),
-    new MiniCssExtractPlugin({
-        filename:'style.[chunkhash].css' //"public/name.pp"
+      filename: '../stats.json', // path.resolve(__dirname, 'dist', 'stats.json')
+      stats: {
+        all: false,
+        assets: true,
+
+      },
     }),
-    
-  ]
+    new MiniCssExtractPlugin({
+      filename: 'style.[chunkhash].css', // "public/name.pp"
+    }),
+
+  ],
 };
 
 // const path = require('path');
--- webpack.config.server.js
+++ webpack.config.server.js
@@ -6,42 +6,42 @@ const nodeExternals = require('webpack-node-externals');
 module.exports = {
   mode: 'production',
   entry: './src/server.js',
-  target: "node",
-  devtool: "source-map",
+  target: 'node',
+  devtool: 'source-map',
   externals: [nodeExternals({
-      whitelist:[/\.(?!(?:jsx?|json)$).{1,5}$/i],
+    whitelist: [/\.(?!(?:jsx?|json)$).{1,5}$/i],
   })],
   output: {
-      path: path.resolve(__dirname, 'dist'),
-      filename: 'server.bundle.js'
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'server.bundle.js',
   },
   module: {
-      rules: [
-          {
-              test: /\.(js|jsx)$/,
-              exclude: /node_modules/,
-              use: 'babel-loader'
-          },
-          {
-            test:/\.css$/,
-            use:'null-loader'
-          },
-          {
-            test: /\.(png|svg|jpg|gif)$/,
-            use: [{loader:'file-loader'}]
-          }
-      ]
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+      {
+        test: /\.css$/,
+        use: 'null-loader',
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [{loader: 'file-loader'}],
+      },
+    ],
   },
   plugins: [
-      
 
-      new webpack.BannerPlugin({
-          banner: 'require("source-map-support").install();',
-          raw: true,
-          entryOnly: false
-      }),
-      
-  ]
+
+    new webpack.BannerPlugin({
+      banner: 'require("source-map-support").install();',
+      raw: true,
+      entryOnly: false,
+    }),
+
+  ],
 };
 
 // const path = require('path');
