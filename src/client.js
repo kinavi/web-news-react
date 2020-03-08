625b2efaa8a1cbd19bbdad3a2ca20d802ab2067b
vm-laptop <wolfmariev@gmail.com>
2020-03-08 13:57:11 +0500

    убрал лишнее

--- src/client.js
+++ src/client.js
@@ -1,26 +1,19 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 import Routes from './Routes';
-import { Provider } from 'react-redux'
-import { BrowserRouter, StaticRouter, HashRouter, Route, Switch } from 'react-router-dom'
-//import 'bootstrap/dist/css/bootstrap.min.css';
-import storeFactory from './store/index'
-import './styles/App.css'
-import { renderRoutes } from 'react-router-config';
-//---
-import MyEditor from './components/EditNewsEditor'
-import AddNews from './components/Forms/AddNewsForm'
-import ListNewsCms from './components/ListNewsCms'
-import __INITIAL_STATE__ from '../data/initialState.json'
+import {Provider} from 'react-redux';
+import {BrowserRouter} from 'react-router-dom';
+import {renderRoutes} from 'react-router-config';
 
-import ListNews from './pages/ListNews'
-const store = storeFactory(false, __INITIAL_STATE__)
-//---
-//const store = storeFactory(false, window.__PRELOADED_STATE__)
+// import 'bootstrap/dist/css/bootstrap.min.css';
+import storeFactory from './store/index';
+import './styles/App.css';
+import __INITIAL_STATE__ from '../data/initialState.json';
 
+const store = storeFactory(false, __INITIAL_STATE__);
+// ---
+// const store = storeFactory(false, window.__PRELOADED_STATE__)
 
-
-//---
 ReactDOM.render(
     <Provider store={store}>
       <BrowserRouter>
@@ -28,6 +21,6 @@ ReactDOM.render(
       </BrowserRouter>
     </Provider>
     ,
-    document.getElementById("root")
-  );
+    document.getElementById('root'),
+);
 
