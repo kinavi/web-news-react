54dc83c30b7a7dceea02750f97858d8f937868cc
vm-laptop <wolfmariev@gmail.com>
2020-03-10 14:08:29 +0500

    Отключил свои стили

--- src/client.js
+++ src/client.js
@@ -5,14 +5,14 @@ import {Provider} from 'react-redux';
 import {BrowserRouter} from 'react-router-dom';
 import {renderRoutes} from 'react-router-config';
 
-// import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import storeFactory from './store/index';
-import './styles/App.css';
-import __INITIAL_STATE__ from '../data/initialState.json';
+// import './styles/App.css';
+// import __INITIAL_STATE__ from '../data/initialState.json';
 
-const store = storeFactory(false, __INITIAL_STATE__);
+// const store = storeFactory(false, __INITIAL_STATE__);
 // ---
-// const store = storeFactory(false, window.__PRELOADED_STATE__)
+const store = storeFactory(false, window.__PRELOADED_STATE__);
 
 ReactDOM.render(
     <Provider store={store}>
