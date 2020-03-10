398093684a045843dc411306f7a060b5eb70ab76
vm-laptop <wolfmariev@gmail.com>
2020-03-10 14:26:27 +0500

    Пробелы, кавычки, комменты

--- src/components/ListNewsCms.js
+++ src/components/ListNewsCms.js
@@ -1,56 +1,28 @@
-import React from "react";
-import { connect } from 'react-redux'
-//import NewsTitle from './NewsTitle'
-//import EditNews from './EditNews'
-import { Button } from "react-bootstrap";
-import { removeNews } from '../store/Actions'
-import { Link, useHistory,useParams, useLocation } from 'react-router-dom'
-import { renderRoutes } from 'react-router-config';
+import React from 'react';
+import {connect} from 'react-redux';
 
-const ListNewsCms = ({ ListNews, onRemove}) =>{
+import {NewsSelectorForm} from '.';
 
-    let history = useHistory();
+const ListNewsCms = ({News}) =>{
 
-    function handleClickChangeBtn(id){
-        history.push(`/edit/${id}`);
-    }
+  return (
+    <div className="">
+      {(!!News)?
+        News.map((item, i)=>
+          <NewsSelectorForm key={i} {...item} />):
+        <div>Новостей нет</div>
+      }
 
-    return(
-        <div className="">
-            {(!!ListNews)?
-                ListNews.map((n,i)=>
-                    <div key={i} className="shadow mb-2">
-                        <div style={{background:"#af9b79"}} className="d-flex rounded">
-                            <div className="align-self-center ml-2 rounded text-white flex-fill">{n.title}</div>
-                            <Button onClick={()=>handleClickChangeBtn(n.id)} size="sm" className="m-2">Изменить</Button> 
-                            <Button onClick={() => onRemove(n.id)} variant="danger" size="sm" className="m-2">Удалить</Button>
-                        </div>
-                    </div>
-                    
-                    )
-                :
-                <div>Новостей нет</div>
-            }
-            
-        </div>
-    )
-}
-        
+    </div>
+  );
+};
 
-const mapStateToProps = state =>({
-    ListNews:state.ListNews
-})
 
-const mapDispatchToProps = dispatch =>({
-    // onEdit(id, title, description){
-    //     dispatch(editNews(id, title, description)) {pathname:"/cms/edit", search:`?id=${n.id}`}
-    // },
-    onRemove(id){
-        dispatch(removeNews(id))
-    }
+const mapStateToProps = (state) =>({
+  News: state.News,
+});
 
-})
 export default connect(
     mapStateToProps,
-    mapDispatchToProps
-)(ListNewsCms)
+    null,
+)(ListNewsCms);
