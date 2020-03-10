4190ec1f484f34a6f4ca5b2d36c0959a2d460001
vm-laptop <wolfmariev@gmail.com>
2020-03-10 14:33:59 +0500

    Добавил новый редусер

--- src/store/Redusers/News.js
+++ src/store/Redusers/News.js
@@ -1,24 +1,24 @@
-import { TypeActions } from '../TypeActions'
+import {TypeActions} from '../TypeActions';
 
-export const News = (state = {}, action) =>
-{
-    switch(action.type)
-    {
-        case TypeActions.ADD_NEWS:
-            return {
-                id:action.id,
-                title:action.title,
-                description:action.description,
-                fileName:action.fileName
-            }
-        case TypeActions.EDIT_NEWS:
-            return{
-                id:action.id,
-                title:action.title,
-                description:action.description,
-                fileName:action.fileName
-            }
-        default:
-            return state
-    }
-}
+export const News = (state = [], action) =>{
+  switch (action.type) {
+    case TypeActions.ADD_NEWS:
+      return [{
+        id: action.id,
+        title: action.title,
+        description: action.description,
+        fileName: action.fileName,
+      }, ...state];
+    case TypeActions.EDIT_NEWS:
+      return state.map((itme) => itme.id==action.id?{
+        ...itme,
+        title: action.title,
+        description: action.description,
+        fileName: action.fileName,
+      }:itme);
+    case TypeActions.REMOVE_NEWS:
+      return state.filter((item)=>item.id!=action.id);
+    default:
+      return state;
+  }
+};
