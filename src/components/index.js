45c8d8a7f701e40227ba56ceef8202591f24ed41
vm-laptop <wolfmariev@gmail.com>
2020-03-10 14:22:18 +0500

    Добавил файл контейнер для компонентов

---
+++ src/components/index.js
@@ -0,0 +1,23 @@
+import {NewsPreview} from './NewsPreview';
+import ListNewsMain from './ListNewsMain';
+import {Page} from './Page';
+import {EditorPreview} from './editor/EditorPreview';
+import {EditorNews} from './editor/EditorNews';
+import ListNewsCms from './ListNewsCms';
+import {ButtonSelectImg, TitleField} from './FormElements';
+import NewsLite from './NewsLite';
+import NewsEdit from './NewsEdit';
+import {NewsSelectorForm} from './NewsSelectorForm';
+export {
+  NewsPreview,
+  ListNewsMain,
+  Page,
+  EditorPreview,
+  EditorNews,
+  ListNewsCms,
+  ButtonSelectImg,
+  TitleField,
+  NewsLite,
+  NewsEdit,
+  NewsSelectorForm,
+};
