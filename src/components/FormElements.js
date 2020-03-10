20ae3e822e0a3d51785c7f7bdb552f38721cb190
vm-laptop <wolfmariev@gmail.com>
2020-03-10 14:17:51 +0500

    Убрал не используемые элементы

--- src/components/FormElements.js
+++ src/components/FormElements.js
@@ -1,29 +1,35 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState} from 'react';
 
-import { IoIosArrowDropleft } from "react-icons/io";
-import IMG from './IMG'
+import IMG from './IMG';
 
-export const TitleField = ({value = "", setValue}) =>
-        <div className="form-group">
-            <label className="exampleInputPassword1">Заголовок</label>
-            <input  style={{background:"#e3edef"}}  className="form-control" onChange={(e)=>setValue(e.target.value)} value = {value}/>
-        </div> 
+export const TitleField = ({value = '', setValue}) =>
+  <div className="form-group">
+    <label className="exampleInputPassword1">Заголовок</label>
+    <input
+      style={{background: '#e3edef'}}
+      className="form-control"
+      onChange={(e)=>setValue(e.target.value)}
+      value = {value}/>
+  </div>;
 
-export const ButtonSelectImg = ({ value, setValue}) =>{
+export const ButtonSelectImg = ({value, setValue}) =>{
+  const [refFileInp, setRefFileInp] = useState();
 
-    const [refFileInp, setRefFileInp] = useState();
-
-    return(
+  return (
+    <div>
+      <input
+        ref={(node)=>setRefFileInp(node)}
+        style={{display: 'none'}}
+        type="file"
+        name="filedata"
+        onChange={(e)=>setValue(e.target.files[0])}/>
+      <div className="form-group">
+        <label className="exampleInputPassword1">Картинка</label>
         <div>
-            <input ref={node=>setRefFileInp(node)} style={{display:"none"}} type="file" name="filedata" onChange={(e)=>setValue(e.target.files[0])}/>
-            <div className="form-group">
-                <label className="exampleInputPassword1">Картинка</label>
-                    <div>
-                        {!!value?<IMG file={value}/>:<div></div>}
-                    </div> 
-                    <button onClick={()=>refFileInp.click()}>Выберите картинку</button>
-            </div>
+          {!!value?<IMG file={value}/>:<div></div>}
         </div>
-    )
-
-}
+        <button onClick={()=>refFileInp.click()}>Выберите картинку</button>
+      </div>
+    </div>
+  );
+};
