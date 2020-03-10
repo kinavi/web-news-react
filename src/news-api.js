ee83a6f7dba463fe1a6b6f08655caedcb919507d
vm-laptop <wolfmariev@gmail.com>
2020-03-10 14:09:31 +0500

    Убрал лишнии пробелы, двойные кавычки

--- src/news-api.js
+++ src/news-api.js
@@ -1,82 +1,82 @@
-import { Router } from 'express'
-import { TypeActions } from './store/TypeActions'
-import { v4 } from 'uuid'
-var multer  = require('multer')
-//var upload = multer({ dest: 'uploads/' })
+import {Router} from 'express';
+import {TypeActions} from './store/TypeActions';
+import {v4} from 'uuid';
+const multer = require('multer');
+// var upload = multer({ dest: 'uploads/' })
 
 const storageConfig = multer.diskStorage({
-    destination: (req, file, cb) =>{
-        cb(null, "uploads");
-    },
-    filename: (req, file, cb) =>{
-        cb(null, file.originalname);
-    }
+  destination: (req, file, cb) =>{
+    cb(null, 'uploads');
+  },
+  filename: (req, file, cb) =>{
+    cb(null, file.originalname);
+  },
 });
 
-const router = Router()
+const router = Router();
 
 const dispatchAndRespond = (req, res, action) => {
-    req.store.dispatch(action)
-    res.status(200).json(action)
-}
-    
+  req.store.dispatch(action);
+  res.status(200).json(action);
+};
+
 
 // router.get("/cms", (req, res) =>{
 //     res.status(200).json(req.store.getState().ListNews)
 //     })
 
-router.get("/", (req, res) =>{
-    res.status(200).json(req.store.getState().ListNews)
-    })
+router.get('/', (req, res) =>{
+  res.status(200).json(req.store.getState().ListNews);
+});
 
-router.post("/cms", (req, res) =>
+router.post('/cms', (req, res) =>
 
-    dispatchAndRespond(req, res, {
-        type: TypeActions.ADD_NEWS,
-        id: v4(),
-        title: req.body.title,
-        description: req.body.description,
-        fileName:req.body.fileName,
-    })
-)
+  dispatchAndRespond(req, res, {
+    type: TypeActions.ADD_NEWS,
+    id: v4(),
+    title: req.body.title,
+    description: req.body.description,
+    fileName: req.body.fileName,
+  }),
+);
 
-router.use(multer({storage:storageConfig}).any());
+router.use(multer({storage: storageConfig}).any());
 router.post('/upload', (req, res, next)=>{
+  const filedata = req.files;
+  console.log(filedata);
+  if (!filedata) {
+    res.send('Ошибка при загрузке файла');
+  } else {
+    res.send('Файл загружен');
+  }
+});
 
-    let filedata = req.files;
-    console.log(filedata);
-    if(!filedata)
-        res.send("Ошибка при загрузке файла");
-    else
-        res.send("Файл загружен");
-})
+router.put('/cms', (req, res) =>
+  dispatchAndRespond(req, res, {
+    type: TypeActions.EDIT_NEWS,
+    id: req.body.id,
+    title: req.body.title,
+    description: req.body.description,
+    fileName: req.body.fileName,
+  }),
+);
 
-router.put("/cms", (req, res) =>
-    dispatchAndRespond(req, res, {
-        type: TypeActions.EDIT_NEWS,
-        id: req.body.id,
-        title: req.body.title,
-        description: req.body.description,
-        fileName: req.body.fileName
-    })
-)
+router.delete('/cms', (req, res) =>
+// Logger(req.body.id)(req, res, {
+//     type: TypeActions.REMOVE_NEWS,
+//     id: req.body.id
+// })
+{
+  console.log(req.body);
+  dispatchAndRespond(req, res, {
+    type: TypeActions.REMOVE_NEWS,
+    id: req.body.id,
+  });
+},
 
-router.delete("/cms", (req, res) =>
-    // Logger(req.body.id)(req, res, {
-    //     type: TypeActions.REMOVE_NEWS,
-    //     id: req.body.id
-    // })
-    {
-        console.log(req.body)
-        dispatchAndRespond(req, res, {
-            type: TypeActions.REMOVE_NEWS,
-            id: req.body.id
-        })
-    }
+);
 
-)
-
-export default router
+export default router;
 
 // const Logger = message => (req, res, action)  => {
 //     console.log(message);
