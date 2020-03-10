1fac6d071c0024b8a355ff44d1feb58787590d10
vm-laptop <wolfmariev@gmail.com>
2020-03-10 14:38:32 +0500

    Убрали лишние пробелы, двойные кавычки

--- src/pages/News.js
+++ src/pages/News.js
@@ -1,32 +1,31 @@
-import React, {useState, useEffect} from "react";
-import { Page } from '../components/Page'
-import { useParams } from "react-router-dom";
-import { connect } from 'react-redux'
-import FullNews from '../components/FullNews'
+import React, {useState, useEffect} from 'react';
+import {Page} from '../components/Page';
+import {useParams} from 'react-router-dom';
+import {connect} from 'react-redux';
+import FullNews from '../components/FullNews';
 
 const News = ({ListNews}) =>{
+  const {id} = useParams();
+  const [News, setNews] = useState(getItemById(ListNews, id));
 
-    let { id } = useParams();
-    const [News, setNews] = useState(getItemById(ListNews, id))
-    
-    return(
-        <Page>
-            <FullNews {...News}/>
-        </Page>
-    )
-}
-const mapStateToProps = state =>({
-    ListNews:state.ListNews
-})
+  return (
+    <Page>
+      <FullNews {...News}/>
+    </Page>
+  );
+};
+const mapStateToProps = (state) =>({
+  ListNews: state.ListNews,
+});
 
 export default {
-    component:connect(
-        mapStateToProps,
-        null
-    )(News)
-}
+  component: connect(
+      mapStateToProps,
+      null,
+  )(News),
+};
 
 // Фигня, надо переделать
 const getItemById = (List, id) =>{
-    return List.filter(item => item.id==id)[0] 
-}
+  return List.filter((item) => item.id==id)[0];
+};
