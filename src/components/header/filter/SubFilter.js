99a89b111e8d3a1b19b0cebd339cfee6f98f8250
vm-laptop <wolfmariev@gmail.com>
2020-03-25 21:04:05 +0500

    Реорганизация компонентов фильтра

---
+++ src/components/header/filter/SubFilter.js
@@ -0,0 +1,19 @@
+import React, {useState, useContext} from 'react';
+import {Select} from './Select';
+import {author} from '../../../../data/dataFilter';
+export const SubFilter = () =>{
+  const style = {
+    background: '#181a19',
+    height: '100px',
+    wight: '100%',
+    outline: '2px solid #333333',
+    marginTop: '2px',
+    display: 'flex',
+  };
+    // const {modalState, setStateModal} = useContext(ModalContext);
+  return (
+    <div style={style}>
+      <Select title='Author' values={author}/>
+    </div>
+  );
+};
---
+++ src/components/header/filter/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import {IconContext} from 'react-icons';
+import {MdFilterList} from 'react-icons/md';
+
+import {Select} from './Select';
+import {SubFilter} from './SubFilter';
+
+const Filter = ({onClick}) =>{
+  return (
+    <button onClick={onClick} className='header__btn header__search'>
+      <IconContext.Provider value={{size: 30, className: 'header__icon'}}>
+        <MdFilterList/>
+      </IconContext.Provider>
+    </button>
+  );
+};
+export {Filter, Select, SubFilter};
