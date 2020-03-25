131122b43b2d0c3c82de951881224bd3d827a172
vm-laptop <wolfmariev@gmail.com>
2020-03-25 21:02:44 +0500

    Легкая чистка

--- src/components/header/Search.js
+++ src/components/header/Search.js
@@ -2,13 +2,22 @@ import React from 'react';
 
 import {IconContext} from 'react-icons';
 import {GoSearch} from 'react-icons/go';
+import {MdClose} from 'react-icons/md';
 
-export const Search = () =>{
+import './Search.css';
+
+export const Search = ({isSearch, setSearch}) =>{
   return (
-    <button className='header__btn header__search'>
+    <button className='header__btn header__search' onClick={()=>setSearch(!isSearch)}>
+      {isSearch?
       <IconContext.Provider value={{size: 30, className: 'header__icon'}}>
         <GoSearch/>
+      </IconContext.Provider>:
+      <IconContext.Provider value={{size: 30, className: 'header__icon'}}>
+        <MdClose/>
       </IconContext.Provider>
+      }
+
     </button>
   );
 };
