47e2925e6b3cea29107875e128d8c333e9c158f5
vm-laptop <wolfmariev@gmail.com>
2020-03-25 20:58:15 +0500

    Обновили анимацию, рефакторинг

--- src/components/Page.js
+++ src/components/Page.js
@@ -1,36 +1,60 @@
 import React, {useState} from 'react';
+import {connect} from 'react-redux';
+import {CSSTransition} from 'react-transition-group';
+
 import {Auth, Header, Home, Menu, Search, Title} from './header';
 import {Footer} from './footer';
+import {Filter} from './header/filter';
+import {SubHeader} from './header/SubHeader';
 
-import {ModalContext} from './context';
+import './Page.css';
+import './header/animation/slide.css';
 
-// import '../styles/App.css';
-import '../styles/Header.css';
-import '../styles/Login.css';
+const Page = ({isAuth, children}) =>{
+  const [isMenu, setMenu] = useState(false);
+  const [isVisibleSubMenu, setVisibleSubMenu] = useState(false);
+  const [isVisibleSubFilter, setVisibleSubFilter] = useState(false);
+  const [isVisibleSearch, setVisibleSearch] = useState(true);
 
+  const handlerClickMenu = () =>{
+    setVisibleSubMenu(!isVisibleSubMenu);
+    setMenu(true);
+    if (!isVisibleSubMenu&&isVisibleSubFilter) {
+      setVisibleSubFilter(false);
+    }
+  };
 
-export const Page = ({children}) =>{
-  const [modalState, setStateModal] = useState(false);
+  const handlerClickFilter = () =>{
+    setVisibleSubFilter(!isVisibleSubFilter);
+    setMenu(false);
+    if (!isVisibleSubFilter&&isVisibleSubMenu) {
+      setVisibleSubMenu(false);
+    }
+  };
 
   return (
-    <ModalContext.Provider value={{modalState, setStateModal}}>
-      <div className="page">
-        {/* <ModalLayer/> */}
-        <Header>
-          {/* <Menu/> */}
-          <Home/>
-          <Auth/>
-          <Title/>
-          <Search/>
-        </Header>
-        <div className="container-news">
-          {children}
-        </div>
-        <Footer>
-          Footer
-        </Footer>
+    <div className="page">
+      <Header>
+        <Menu visible={isVisibleSubMenu} onClick={handlerClickMenu} />
+        <Home/>
+        <Title isSearch={isVisibleSearch}/>
+        <Search isSearch={isVisibleSearch} setSearch={setVisibleSearch}/>
+        <Filter onClick={handlerClickFilter}/>
+      </Header>
+      <CSSTransition in={isVisibleSubMenu||isVisibleSubFilter} unmountOnExit timeout={300} classNames='slide' >
+        <SubHeader isMenu={isMenu}/>
+      </CSSTransition>
+      <div className="container-news">
+        {children}
       </div>
-    </ModalContext.Provider>
+      <Footer>
+          Footer
+      </Footer>
+    </div>
   );
 };
+const mapStateToProps = (state) =>({
+  isAuth: state.Auth.isAuth,
+});
 
+export default connect( mapStateToProps, null)(Page);
