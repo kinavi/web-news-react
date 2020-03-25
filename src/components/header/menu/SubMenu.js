b18262ec11ec288e398812fe508698190eddd334
vm-laptop <wolfmariev@gmail.com>
2020-03-25 22:23:17 +0500

    Реорганизация компонента menu

--- src/components/header/menu/SubMenu.js
+++ src/components/header/menu/SubMenu.js
@@ -2,19 +2,24 @@ import React, {useState} from 'react';
 import Login from '../../forms/Login';
 
 import './SubMenu.css';
+import '../animation/fade.css';
 
-export const SubMenu = ({children}) =>{
+import {CSSTransition} from 'react-transition-group';
+
+export const SubMenu = ({visible, children}) =>{
   const [isVisibleLogin, setVisibleLogin] =useState(false);
   const handlerLogin = () =>{
     setVisibleLogin(!isVisibleLogin);
   };
   return (
     <div className='sub-menu'>
-      <div className='sub-menu__links-container'>
-        {children}
-      </div>
-      <button className='header__link' onClick={handlerLogin} >Login</button>
+        <div className='sub-menu__links-container'>
+          {children}
+        </div>
+        <button className='header__link' onClick={handlerLogin}>Login</button>
       {isVisibleLogin&&<Login onClose={()=>setVisibleLogin(false)} />}
+
+
     </div>
   );
 };
---
+++ src/components/header/menu/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {IconContext} from 'react-icons';
+import {GiHamburgerMenu} from 'react-icons/gi';
+import {MdClose} from 'react-icons/md';
+
+import './Menu.css';
+import {SubMenu} from './SubMenu';
+
+const CloseState = () =>
+  <IconContext.Provider
+    value={{size: 30, className: 'header__icon'}}>
+    <GiHamburgerMenu/>
+  </IconContext.Provider>;
+
+const OpenState = () =>
+  <IconContext.Provider
+    value={{size: 30, className: 'header__icon'}}>
+    <MdClose/>
+  </IconContext.Provider>;
+
+const Menu = ({onClick, visible}) =>{
+  return (
+    <button className='menu header__btn' onClick={onClick}>
+      {visible?<OpenState/>:<CloseState/>}
+    </button>
+  );
+};
+
+export {Menu, SubMenu};
