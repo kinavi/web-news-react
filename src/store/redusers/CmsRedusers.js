ef0038d975e33c00c8b2a6d5d7e80e0bf021438c
vm-laptop <wolfmariev@gmail.com>
2020-03-31 20:07:46 +0500

    Новый редусер для страницы cms

---
+++ src/store/redusers/CmsRedusers.js
@@ -0,0 +1,38 @@
+import {fetchThenDispatch} from '../fetchs';
+import {NewsRedusers} from './NewsRedusers';
+
+const LOAD_NEWS_BY_ID = 'LOAD_NEWS_BY_ID';
+const ADD_NEWS = 'ADD_NEWS';
+const EDIT_NEWS = 'EDIT_NEWS';
+const REMOVE_NEWS = 'REMOVE_NEWS';
+
+const initialState = {
+  news: [],
+};
+
+export const CmsRedusers = (state = initialState, action) =>{
+  switch (action.type) {
+    case LOAD_NEWS_BY_ID:
+      return {news: [...action.data]};
+    case ADD_NEWS:
+      return {news: NewsRedusers(state.news, action)};
+    case EDIT_NEWS:
+      return {news: NewsRedusers(state.news, action)};
+    case REMOVE_NEWS:
+      return {news: NewsRedusers(state.news, action)};
+    default: return state;
+  }
+};
+
+export const loadNewsByIdAC = (data) =>({
+  type: LOAD_NEWS_BY_ID,
+  data,
+});
+
+export const getNewsById = (id) => (dispatch) =>
+  fetchThenDispatch(
+      dispatch,
+      '/api/cms',
+      'POST',
+      JSON.stringify({userId: id}),
+  );
